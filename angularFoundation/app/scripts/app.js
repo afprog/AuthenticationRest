bec6f93aa3e8485b83b1485530443946ae6bbb12
afprog <aalzate86@gmail.com>
2014-05-22 17:17:26 -0500

    primer commit

---
+++ angularFoundation/app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('angularFoundationApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
